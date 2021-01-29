e98a6e5210beefd5f0c1dd5cbaf4556ac23b1b69
Coding Bird <Codingbird@yahoo.com>
2021-01-29 22:40:09 +1200

    code refactored

--- script.js
+++ script.js
@@ -1,55 +1,45 @@
-// captureId
-function captureId(idName) {
-  return document.getElementById(idName);
-}
-// item quantity increase function
-function itemQuantityIncrease(idName, inputName, phoneId, phonePrice) {
-    captureId(idName).addEventListener("click", function () {
-    let itemQuantityCount = parseInt(captureId(inputName).value);
-    captureId(inputName).value = ++itemQuantityCount;
-    priceIncrease(phoneId, itemQuantityCount, phonePrice);
-    checkOutPrizeUpdater()
-  });
-}
-itemQuantityIncrease("firstItemIncrease","firstItemInputValue","PhonePrize","1219");
-itemQuantityIncrease("secondItemIncrease","secondItemInputValue","casePrize","59");
-// item quantity decrease function
-function itemQuantityDecrease(idName, inputName, phoneId, phonePrice) {
-    captureId(idName).addEventListener("click", function () {
-        let itemQuantityCount = parseInt(captureId(inputName).value);
-        if (itemQuantityCount > 0) {
-        captureId(inputName).value = --itemQuantityCount;
-        priceIncrease(phoneId, itemQuantityCount, phonePrice);
-        checkOutPrizeUpdater()
-        }
-      });
-}
-//item decrease
-itemQuantityDecrease("firstItemDecrease","firstItemInputValue","PhonePrize","1219");
-itemQuantityDecrease("secondItemDecrease","secondItemInputValue","casePrize","59");
-// multiply to price update
-function priceIncrease(phonePriceId, itemInputVale, phonePrice) {
-  captureId(phonePriceId).innerText = itemInputVale * parseInt(phonePrice);
-}
-// checkout working area
-function checkOutPrizeUpdater() {
-    let phoneTotalPrice = parseInt(captureId('PhonePrize').innerText) + parseInt(captureId('casePrize').innerText);
-    captureId('subTotal').innerText = phoneTotalPrice
-    // taxCalculator
-    const taxValue = (5 / 100).toFixed(2);
-    let generatedTaxValue = parseFloat((taxValue * parseInt(captureId('subTotal').innerText)).toFixed(2));
-    captureId('checkOutTax').innerText = generatedTaxValue;
-    // taxWithTotal calculator
-    captureId('totalWithTax').innerText = phoneTotalPrice + generatedTaxValue;
-}
-// click to item remove || not remove display none done
-function itemsRemove(itemRemoveIdName,rmParentId,phoneQuantity,phonePrice) {
-    captureId(itemRemoveIdName).addEventListener('click', (event) => {
-        captureId(phoneQuantity).innerText = 0;
-        captureId(phonePrice).innerText = 0;
-        checkOutPrizeUpdater();
-        captureId(rmParentId).style.display = 'none';
-    })
-}
-itemsRemove("firstRemove",'cartItemOne','firstItemInputValue','PhonePrize')
-itemsRemove('secondRemove','cartItemTwo','secondItemInputValue','casePrize')
+  // item quantity increase function
+  function handleItemQuantity(idName, inputName, phoneId, productPrice,isIncrease) {
+      document.getElementById(idName).addEventListener("click", function () {
+      let itemQuantityCount = parseInt(document.getElementById(inputName).value);
+      if (isIncrease == false) {
+            itemQuantityCount > 0 ? document.getElementById(inputName).value = --itemQuantityCount : null;
+          } else {
+            document.getElementById(inputName).value = ++itemQuantityCount;
+          }
+      productMoneyUpdater(phoneId, itemQuantityCount, productPrice);
+      checkOutPrizeUpdater();
+    });
+  }
+// item increase
+  handleItemQuantity("firstItemIncrease","firstItemInputValue","PhonePrize","1219",true);
+  handleItemQuantity("secondItemIncrease","secondItemInputValue","casePrize","59",true);
+  //item decrease
+  handleItemQuantity("firstItemDecrease","firstItemInputValue","PhonePrize","1219",false);
+  handleItemQuantity("secondItemDecrease","secondItemInputValue","casePrize","59",false);
+  // multiply to price update
+  function productMoneyUpdater(productPriceId, itemInputVale, productPrice) {
+    document.getElementById(productPriceId).innerText = itemInputVale * parseInt(productPrice);
+  }
+  // checkout working area
+  function checkOutPrizeUpdater() {
+      let phoneTotalPrice = parseInt(document.getElementById('PhonePrize').innerText) + parseInt(document.getElementById('casePrize').innerText);
+      document.getElementById('subTotal').innerText = phoneTotalPrice
+      // taxCalculator
+      const taxValue = (5 / 100).toFixed(2);
+      let generatedTaxValue = parseFloat((taxValue * parseInt(document.getElementById('subTotal').innerText)).toFixed(2));
+      document.getElementById('checkOutTax').innerText = generatedTaxValue;
+      // taxWithTotal calculator
+      document.getElementById('totalWithTax').innerText = phoneTotalPrice + generatedTaxValue;
+  }
+  // click to item remove || not remove display none done
+  function itemsRemove(itemRemoveIdName,deleteParentId,productQuantity,productPrice) {
+      document.getElementById(itemRemoveIdName).addEventListener('click', (event) => {
+          document.getElementById(productQuantity).innerText = 0;
+          document.getElementById(productPrice).innerText = 0;
+          checkOutPrizeUpdater();
+          document.getElementById(deleteParentId).style.display = 'none';
+      })
+  }
+  itemsRemove("firstRemove",'cartItemOne','firstItemInputValue','PhonePrize')
+  itemsRemove('secondRemove','cartItemTwo','secondItemInputValue','casePrize')
---
+++ script1.js
@@ -0,0 +1,56 @@
+// captureId
+//easy to write
+function captureId(idName) {
+  return document.getElementById(idName);
+}
+// item quantity increase function
+function itemQuantityIncrease(idName, inputName, phoneId, phonePrice) {
+    captureId(idName).addEventListener("click", function () {
+    let itemQuantityCount = parseInt(captureId(inputName).value);
+    captureId(inputName).value = ++itemQuantityCount;
+    priceIncrease(phoneId, itemQuantityCount, phonePrice);
+    checkOutPrizeUpdater()
+  });
+}
+itemQuantityIncrease("firstItemIncrease","firstItemInputValue","PhonePrize","1219");
+itemQuantityIncrease("secondItemIncrease","secondItemInputValue","casePrize","59");
+// item quantity decrease function
+function itemQuantityDecrease(idName, inputName, phoneId, phonePrice) {
+    captureId(idName).addEventListener("click", function () {
+        let itemQuantityCount = parseInt(captureId(inputName).value);
+        if (itemQuantityCount > 0) {
+        captureId(inputName).value = --itemQuantityCount;
+        priceIncrease(phoneId, itemQuantityCount, phonePrice);
+        checkOutPrizeUpdater()
+        }
+      });
+}
+//item decrease
+itemQuantityDecrease("firstItemDecrease","firstItemInputValue","PhonePrize","1219");
+itemQuantityDecrease("secondItemDecrease","secondItemInputValue","casePrize","59");
+// multiply to price update
+function priceIncrease(phonePriceId, itemInputVale, phonePrice) {
+  captureId(phonePriceId).innerText = itemInputVale * parseInt(phonePrice);
+}
+// checkout working area
+function checkOutPrizeUpdater() {
+    let phoneTotalPrice = parseInt(captureId('PhonePrize').innerText) + parseInt(captureId('casePrize').innerText);
+    captureId('subTotal').innerText = phoneTotalPrice
+    // taxCalculator
+    const taxValue = (5 / 100).toFixed(2);
+    let generatedTaxValue = parseFloat((taxValue * parseInt(captureId('subTotal').innerText)).toFixed(2));
+    captureId('checkOutTax').innerText = generatedTaxValue;
+    // taxWithTotal calculator
+    captureId('totalWithTax').innerText = phoneTotalPrice + generatedTaxValue;
+}
+// click to item remove || not remove display none done
+function itemsRemove(itemRemoveIdName,rmParentId,phoneQuantity,phonePrice) {
+    captureId(itemRemoveIdName).addEventListener('click', (event) => {
+        captureId(phoneQuantity).innerText = 0;
+        captureId(phonePrice).innerText = 0;
+        checkOutPrizeUpdater();
+        captureId(rmParentId).style.display = 'none';
+    })
+}
+itemsRemove("firstRemove",'cartItemOne','firstItemInputValue','PhonePrize')
+itemsRemove('secondRemove','cartItemTwo','secondItemInputValue','casePrize')
